a76d67d3f140fd17bbd2f91cdfe834a35f4bb3bc
Carlos Camacho <ccamacho@redhat.com>
2019-10-27 09:13:47 -0400

    fix flake and md check

--- pystol-operator/tests/test_sample.py
+++ pystol-operator/tests/test_sample.py
@@ -1,9 +1,31 @@
-import pytest
+#!/usr/bin/env python
+
+"""
+Copyright 2019 Pystol (pystol.org).
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may
+not use this file except in compliance with the License. You may obtain
+a copy of the License at:
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+License for the specific language governing permissions and limitations
+under the License.
+"""
+
+
 def test_file1_method1():
-	x=5
-	y=6
-	assert x+1 == y,"test failed"
+    """Test file 1 method 1."""
+    x = 5
+    y = 6
+    assert x + 1 == y, "test failed"
+
+
 def test_file1_method2():
-	x=5
-	y=6
-	assert x+1 == y,"test failed" 
+    """Test file 1 method 2."""
+    x = 5
+    y = 6
+    assert x + 1 == y, "test failed"
