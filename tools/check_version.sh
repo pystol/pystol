01b7ac1346466622e036e219715ac9216221f4ce
Carlos Camacho <ccamacho@redhat.com>
2019-11-18 19:05:12 +0100

    Add version to Helm chart

--- tools/check_version.sh
+++ tools/check_version.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 PYSTOL_OPERATOR_VERSION=$(cat ./pystol-operator/setup.py | grep "_REVISION = '" | cut -d"'" -f2)
+PYSTOL_HELM_VERSION=$(cat ./helm/Chart.yaml | grep version | cut -d"'" -f2)
 PYSTOL_UI_VERSION=$(cat ./pystol-ui/package.json | grep version | cut -d'"' -f4)
- 
-if [ "$PYSTOL_OPERATOR_VERSION" == "$PYSTOL_UI_VERSION" ]; then
+
+if [[ "$PYSTOL_OPERATOR_VERSION" == "$PYSTOL_HELM_VERSION" && "$PYSTOL_OPERATOR_VERSION" == "$PYSTOL_UI_VERSION" ]]; then
     echo "Version match"
 else
     echo "Version do not match"
