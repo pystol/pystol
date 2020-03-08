393fc752999f4577ad44d7648eb5c6ef7c025596
Carlos Camacho <ccamacho@redhat.com>
2020-03-08 13:41:15 +0100

    Add setuptools version

--- pystol-operator/setup.py
+++ pystol-operator/setup.py
@@ -28,7 +28,7 @@ if version_info < (3, 5):
 
 _NAME = 'pystol'
 _DESCRIPTION = 'The Pystol CLI'
-_REVISION = '0.5.5'
+_REVISION = '0.5.6'
 
 pystol_revision = os.environ.get('PYSTOL_REVISION', "")
 if (pystol_revision != ""):
