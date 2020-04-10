e28d033e29d5449967751d9366909f4e3d31de78
agustin <agustin@A796LSL.tcpsi.es>
2020-04-10 19:07:55 +0200

    import function load config in k8sclient

--- pystol-ui/app/base/k8sclient.py
+++ pystol-ui/app/base/k8sclient.py
@@ -13,42 +13,7 @@ import sys
 import urllib
 import kubernetes
 from kubernetes import client, config, watch
-
-
-def load_kubernetes_config():
-    """
-    Load the initial config details.
-
-    We load the config depending where we execute the code from
-    """
-    try:
-        if 'KUBERNETES_PORT' in os.environ:
-            # We set up the client from within a k8s pod
-            kubernetes.config.load_incluster_config()
-        elif 'KUBECONFIG' in os.environ:
-            kubernetes.config.load_kube_config(os.getenv('KUBECONFIG'))
-        else:
-            kubernetes.config.load_kube_config()
-    except Exception as e:
-        message = ("---\n"
-                   "The Python Kubernetes client could not be configured "
-                   "at this time.\n"
-                   "You need a working Kubernetes deployment to make "
-                   "Pystol work.\n"
-                   "Check the following:\n"
-                   "Use the env var KUBECONFIG with the path to your K8s "
-                   "config file like:\n"
-                   "    export KUBECONFIG=~/.kube/config\n"
-                   "Or run Pystol from within the cluster to make use of "
-                   "load_incluster_config.\n"
-                   "Error: " % (e))
-        print(message)
-        print("---")
-        print("The current Pystol version is: %s" % (pystol_version))
-        print(" ")
-        print("Bye...")
-        sys.exit(0)
-
+from app.base.k8s import load_kubernetes_config
 
 
 def state_cluster():
