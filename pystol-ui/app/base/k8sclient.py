a82cf3133f97d67c187a855e11f8ad2e2af4eff6
agustin <agustin@A796LSL.tcpsi.es>
2020-04-10 18:33:46 +0200

    add k8sclient

--- pystol-ui/app/base/k8sclient.py
+++ pystol-ui/app/base/k8sclient.py
@@ -106,13 +106,23 @@ def state_nodes():
             w.stop()
     return ret
 
-"""     for event in w.stream(v1.list_pod_for_all_namespaces, timeout_seconds=10):
-        print("Event: %s %s %s" % (
-            event['type'],
-            event['object'].kind,
-            event['object'].metadata.name)
-        )
+def state_namespaces():
+    # Configs can be set in Configuration class directly or using helper
+    # utility. If no argument provided, the config will be loaded from
+    # default location.
+    config.load_kube_config()
+    v1 = client.CoreV1Api()
+    count = 10
+    w = watch.Watch()
+    ret = []
+    for event in w.stream(v1.list_namespace, timeout_seconds=10):
+        print("Event: %s %s" % (event['type'], event['object'].metadata.name))
         count -= 1
+       # ret.append(event) 
+        ret.append({'type': event['type'],
+                    'name': event['object'].metadata.name,
+                       })
         if not count:
-            w.stop() """
-               
+            w.stop()
+    return ret
+
