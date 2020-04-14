8c52014ba11acd2f213232894b658326fd009c3b
Agustin Jaume <agustinjaume@ideasextraordinarias.es>
2020-04-14 02:55:11 +0200

    add change cluster
    Conflicts:
    	Dockerfile
    	pystol-ui/app/base/k8sclient.py

--- pystol-ui/app/home/routes.py
+++ pystol-ui/app/home/routes.py
@@ -10,19 +10,10 @@ from flask_login import login_required, current_user
 from app import login_manager
 from jinja2 import TemplateNotFound
 from app.base.k8s import list_actions, show_actions
-from app.base.k8sclient import  state_namespaces, state_nodes, state_pods, cluster_name_configured
+from app.base.k8sclient import os, state_namespaces, state_nodes, state_pods, cluster_name_configured
 from app.base.allocated import compute_allocated_resources
 from app.base.hexa import hexagons_data
 
-
-@blueprint.route('/page-colors')
-@login_required
-def colors():
-    #if not current_user.is_authenticated:
-    #    return redirect(url_for('base_blueprint.login'))
-    return render_template('page-colors.html',
-                           compute_allocated_resources = compute_allocated_resources(),
-                          )
 
 @blueprint.route('/index')
 @login_required
@@ -56,12 +47,17 @@ def route_template(template):
     except:
         return render_template('page-500.html'), 500
 
-# pystol-actions-before will be displayed anyway as all the
-# templates are rendered, see 10 lines before.
-# Remove from here, you are skiping the auth
-#pystol-actions-before.html
-#@blueprint.route('/pystol-actions-before', methods=['GET'])
-#def pystol_actions_before():
-#    return render_template( 'pystol-actions-before.html',
-#    actions=jsonify(show_actions()))
-#    #return jsonify(show_actions())
+
+@blueprint.route('/uploadkubeconfig', methods = ['POST'])
+def upload_image():
+    try:
+        if request.method == "POST":
+            file = request.files['kubeconfig']
+            filename = file.filename
+            file.save(os.path.join("/tmp/", filename))
+            os.rename("/tmp/" + filename,'.kube/config') 
+            return redirect( '/pystol-update-kubeconfig')
+    except TemplateNotFound:
+        return render_template('page-404.html'), 404
+    except:
+        return render_template('page-500.html'), 500
