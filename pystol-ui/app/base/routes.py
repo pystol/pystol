97158ad96a4ab1488241f61d14e3a6a479c3b6f0
Carlos Camacho <ccamacho@redhat.com>
2020-04-14 13:11:33 +0000

    Clean paths and remove unused templates

--- pystol-ui/app/base/routes.py
+++ pystol-ui/app/base/routes.py
@@ -132,16 +132,20 @@ def shutdown():
 
 @login_manager.unauthorized_handler
 def unauthorized_handler():
-    return render_template('errors/403.html'), 403
+    # return render_template('errors/403.html'), 403
+    return render_template('page-403.html', template_folder="../home/templates/"), 403
 
 @blueprint.errorhandler(403)
 def access_forbidden(error):
-    return render_template('errors/403.html'), 403
+    # return render_template('errors/403.html'), 403
+    return render_template('page-403.html', template_folder="../home/templates/"), 403
 
 @blueprint.errorhandler(404)
 def not_found_error(error):
-    return render_template('errors/404.html'), 404
+    # return render_template('errors/404.html'), 404
+    return render_template('page-404.html', template_folder="../home/templates/"), 404
 
 @blueprint.errorhandler(500)
 def internal_error(error):
-    return render_template('errors/500.html'), 500
+    # return render_template('errors/500.html'), 500
+    return render_template('page-500.html', template_folder="../home/templates/"), 500
