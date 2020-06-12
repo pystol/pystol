40b83a82151a9962ab422a729cde8c294e19b334
Carlos Camacho <ccamacho@redhat.com>
2020-06-12 13:37:00 +0200

    Bump to 0.8.15

--- pystol-operator/setup.py
+++ pystol-operator/setup.py
@@ -28,7 +28,7 @@ if version_info < (3, 5):
 
 _NAME = 'pystol'
 _DESCRIPTION = 'The Pystol CLI'
-_REVISION = '0.8.9'
+_REVISION = '0.8.15'
 
 pystol_revision = os.environ.get('PYSTOL_REVISION', "")
 if (pystol_revision != ""):
